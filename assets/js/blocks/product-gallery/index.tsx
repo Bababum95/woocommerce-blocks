cdc33d07e8572606a647beee0325bbed0fe700a8
Andrei <116939459+Bababum95@users.noreply.github.com>
2023-12-13 16:08:59 +0700

    gallery

--- assets/js/blocks/product-gallery/index.tsx
+++ assets/js/blocks/product-gallery/index.tsx
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-import { isExperimentalBuild } from '@woocommerce/block-settings';
 import { registerBlockType } from '@wordpress/blocks';
 
 /**
@@ -16,11 +15,9 @@ import './inner-blocks/product-gallery-large-image-next-previous';
 import './inner-blocks/product-gallery-pager';
 import './inner-blocks/product-gallery-thumbnails';
 
-if ( isExperimentalBuild() ) {
-	// @ts-expect-error: `metadata` currently does not have a type definition in WordPress core.
-	registerBlockType( metadata, {
-		icon,
-		edit: Edit,
-		save: Save,
-	} );
-}
+// @ts-expect-error: `metadata` currently does not have a type definition in WordPress core.
+registerBlockType( metadata, {
+	icon,
+	edit: Edit,
+	save: Save,
+} );
